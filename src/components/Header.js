6d9bd40328efe51fa68c02393777827ddd0b34e3
iykecode93 <boscoiyke67@gmail.com>
2023-12-18 23:42:40 +0100

    update

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+
+const Header = () => {
+    return (
+        <div className="ui menu" style={{marginTop: "20px"}}>
+            <div className="ui container center">
+                <h2 style={{paddingLeft: "450px"}}>Contact Manager</h2>
+            </div>
+        </div>
+    );
+};
+  
+export default Header;
